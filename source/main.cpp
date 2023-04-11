85d8e97cfb6c9dc60887e009758f50757985b4c1
marcusmadland <mrmaratcho@gmail.com>
2023-04-11 21:58:35 +0200

    Changed submodule tracking branch

--- source/main.cpp
+++ source/main.cpp
@@ -4,7 +4,8 @@
 #include "mapp/window.hpp"
 #include "mapp/layer.hpp"
 
-#include "mapp/platform-win/window_win.hpp"
+//#include "mapp/platform-win/window_win.hpp"
+#include "mapp/platform-mac/window_mac.hpp"
 
 #include <iostream>
 
@@ -21,9 +22,10 @@ int main(int argc, char** argv)
 {
     std::cout << "Main called." << std::endl;
 
-    mapp::WindowWin* windowsWindow = new mapp::WindowWin("My App", 1280, 720);
+    //mapp::WindowWin* window = new mapp::WindowWin("My App", 1280, 720);
+    mapp::WindowMac* window = new mapp::WindowMac("My App", 1280, 720);
 
-    Application* app = new Application(windowsWindow);
+    Application* app = new Application(window);
     app->run();
 
     return 0;
